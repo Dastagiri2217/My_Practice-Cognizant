cbca3f65c22e932ccbc4f12bdb66014138bceb33
Dastagiri <162585700+Dastagiri2217@users.noreply.github.com>
2025-06-19 09:33:24 +0530

    Mandatory Assignment - 2

--- WEEK1/DesignPatterns/CreationalDesignPattern/FactoryDesignPattern/FactoryMethodPattern.java
+++ WEEK1/DesignPatterns/CreationalDesignPattern/FactoryDesignPattern/FactoryMethodPattern.java
@@ -1,19 +1,19 @@
-interface Document {
+interface Document { // for all types of documents
 	void open();
 }
-class WordDocument implements Document {
+class WordDocument implements Document { // implementing document to override open method for specific type of Doc
 
 	public void open() {
 		System.out.println("Word Document is Opening");
 	}
 }
-class ExcelDocument implements Document {
+class ExcelDocument implements Document {//same as WordDocument
 
 	public void open() {
 		System.out.println("Excel Document is Opening");
 	}
 }
-class PDFDocument implements Document {
+class PDFDocument implements Document { //same as Word Document
 
 	public void open() {
 
@@ -21,7 +21,8 @@ class PDFDocument implements Document {
 	}
 
 }
-class DocumentFactory {
+class DocumentFactory { 
+	// it is Base Idea of this Pattern that is Factory Class Which helps to decide the object creation at runtime without creating object already
 	public static Document CreateDocument(String type) {
 		if(type.equals("word")) {
 			return new WordDocument();
@@ -33,7 +34,7 @@ class DocumentFactory {
 			return new ExcelDocument();
 		}
 		else {
-// 			System.out.println("Unknown Document Type");
+// 			System.out.println("Unknown Document Type"); //Either we can thorow Exception or add anthoer Type of Document like Json etc
 			throw new IllegalArgumentException("Unknown Document Type");
 		}
 	}
